4ea8cdc0e0808a153e9ae5d51f874e793e7ba0cf
sago35 <sago35@gmail.com>
2017-06-12 22:41:44 +0900

    Initial

---
+++ editor.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	runewidth "github.com/mattn/go-runewidth"
+	"github.com/nsf/termbox-go"
+)
+
+type Editor struct {
+	text    []rune
+	cursorx int
+	cursory int
+	width   int
+	height  int
+}
+
+func NewEditor() *Editor {
+	return &Editor{}
+}
+
+func (e *Editor) SetSize(w, h int) {
+	e.width = w
+	e.height = h
+}
+
+func (e *Editor) SetRune(r rune) {
+	e.text = []rune{r}
+}
+
+func (e *Editor) AddRune(r rune) {
+	e.text = append(e.text, r)
+	if r == rune('\n') {
+		e.cursorx = 0
+		e.cursory++
+	} else {
+		e.cursorx += runewidth.RuneWidth(r)
+	}
+}
+
+func (e *Editor) DeletePrevRune() {
+}
+
+func (e *Editor) Draw() {
+	x := 0
+	y := 0
+	for i := 0; i < len(e.text); i++ {
+		if e.text[i] == rune('\n') {
+			x = 0
+			y++
+		} else {
+			if x < e.width {
+				termbox.SetCell(x, y, e.text[i], termbox.ColorDefault, termbox.ColorDefault)
+			}
+			x = x + runewidth.RuneWidth(e.text[i])
+		}
+	}
+}
+
+func (e *Editor) UpdateCursor() {
+	termbox.SetCursor(e.cursorx, e.cursory)
+}
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer termbox.Close()
+
+	e := NewEditor()
+	e.SetSize(termbox.Size())
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+
+mainloop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break mainloop
+			case termbox.KeyArrowLeft, termbox.KeyCtrlB:
+			case termbox.KeyArrowRight, termbox.KeyCtrlF:
+			case termbox.KeyBackspace, termbox.KeyBackspace2:
+			case termbox.KeyDelete, termbox.KeyCtrlD:
+			case termbox.KeyTab:
+			case termbox.KeySpace:
+			case termbox.KeyCtrlK:
+			case termbox.KeyHome, termbox.KeyCtrlA:
+			case termbox.KeyEnd, termbox.KeyCtrlE:
+			case termbox.KeyEnter:
+				e.AddRune(rune('\n'))
+				e.Draw()
+				e.UpdateCursor()
+				termbox.Flush()
+			default:
+				if ev.Ch != 0 {
+					e.AddRune(ev.Ch)
+					e.Draw()
+					e.UpdateCursor()
+					termbox.Flush()
+				}
+			}
+		case termbox.EventResize:
+			e.SetSize(termbox.Size())
+		}
+	}
+}
