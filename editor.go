b978521491e84399452236108a4fc6335f78d39b
sago35 <sago35@gmail.com>
2017-06-13 23:22:17 +0900

    Add x and y for termbox.SetCursor()

--- editor.go
+++ editor.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	runewidth "github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
 
 type Editor struct {
 	text   []rune
+	x      int
+	y      int
 	cursor int
 	width  int
 	height int
 }
 
 func NewEditor() *Editor {
-	return &Editor{}
+	return &Editor{
+		x: 1,
+		y: 1,
+	}
 }
 
 func (e *Editor) SetSize(w, h int) {
@@ -22,16 +29,18 @@ func (e *Editor) SetSize(w, h int) {
 }
 
 func (e *Editor) AddRune(r rune) {
-	if e.cursor < len(e.text) {
-		e.text = append(e.text[:e.cursor], e.text[e.cursor-1:]...)
-		e.text[e.cursor] = r
+	cursor := e.calcCursor()
+	if cursor < len(e.text) {
+		e.text = append(e.text[:cursor], e.text[cursor-1:]...)
+		e.text[cursor] = r
 	} else {
-		e.text = append(e.text[:e.cursor], r)
+		e.text = append(e.text[:cursor], r)
 	}
 	if r == rune('\n') {
-		e.cursor++
+		e.x = 0
+		e.y += 1
 	} else {
-		e.cursor += runewidth.RuneWidth(r)
+		e.x += runewidth.RuneWidth(r)
 	}
 }
 
@@ -40,6 +49,7 @@ func (e *Editor) DeletePrevRune() {
 
 func (e *Editor) Draw() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCursor(e.x-1, e.y-1)
 	x := 0
 	y := 0
 	for i := 0; i < len(e.text); i++ {
@@ -53,20 +63,45 @@ func (e *Editor) Draw() {
 			x = x + runewidth.RuneWidth(e.text[i])
 		}
 	}
-	termbox.SetCursor(e.calcCursorXY())
+	e.debugDraw()
 	termbox.Flush()
+}
+
+func (e *Editor) debugDraw() {
+	str := fmt.Sprintf("x=%d, y=%d, cursor=%d, len(text)=%d", e.x, e.y, e.calcCursor(), len(e.text))
+	for i, r := range []rune(str) {
+		termbox.SetCell(i, e.height-1, r, termbox.ColorDefault, termbox.ColorDefault)
+	}
 }
 
 func (e *Editor) MoveCursor(x, y int) {
 	if x > 0 {
-		if e.cursor+x < len(e.text)+1 {
-			e.cursor += x
+		if e.x+x <= len(e.text)+1 {
+			e.x += x
 		}
 	} else {
-		if 0 <= e.cursor+x {
-			e.cursor += x
+		if 1 <= e.x+x {
+			e.x += x
 		}
 	}
+}
+
+func (e *Editor) calcCursor() int {
+	// e.x と e.y から、[]runeとしての位置を割り出す
+	ri := 0
+	x := 0
+
+	//if e.x != 0 {
+	for _, r := range e.text {
+		if x >= e.x-runewidth.RuneWidth(r) {
+			break
+		}
+		x += runewidth.RuneWidth(r)
+		ri++
+	}
+	//}
+
+	return ri
 }
 
 func (e *Editor) calcCursorXY() (int, int) {
